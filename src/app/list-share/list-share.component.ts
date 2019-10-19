eb25a78c1851a47077a928c250a297d930517d82
Märt Vaha <mart.vaha@elisa.ee>
2019-10-19 19:43:45 +0300

    feat: add email copy-paste to share component

--- src/app/list-share/list-share.component.ts
+++ src/app/list-share/list-share.component.ts
@@ -1,39 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import { map } from 'rxjs/operators';
-import { COMMA, ENTER } from '@angular/cdk/keycodes';
-import { ActivatedRoute, ParamMap } from '@angular/router';
-import { Observable } from 'rxjs';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { MatChipInputEvent } from '@angular/material/chips';
+import { Component, OnInit } from "@angular/core";
+import { map } from "rxjs/operators";
+import { COMMA, ENTER, SEMICOLON, FF_SEMICOLON } from "@angular/cdk/keycodes";
+import { ActivatedRoute, ParamMap } from "@angular/router";
+import { Observable } from "rxjs";
+import { AngularFirestore } from "@angular/fire/firestore";
+import { MatChipInputEvent } from "@angular/material/chips";
+import { FormControl, Validators, ValidationErrors } from "@angular/forms";
+import { User } from "../auth/auth.service";
 
 @Component({
-  selector: 'app-list-share',
-  styleUrls: ['./list-share.component.scss'],
-  templateUrl: './list-share.component.html'
+  selector: "app-list-share",
+  styleUrls: ["./list-share.component.scss"],
+  templateUrl: "./list-share.component.html"
 })
 export class ListShareComponent implements OnInit {
+  emailControl = new FormControl(undefined, [
+    Validators.required,
+    Validators.email
+  ]);
+  users: Partial<User>[] = [];
   private listId: Observable<string>;
-  public users: any[] = [];
   constructor(private route: ActivatedRoute, private db: AngularFirestore) {}
-  readonly separatorKeysCodes = [ENTER, COMMA];
+
+  readonly separatorKeysCodes = [ENTER, COMMA, FF_SEMICOLON];
+  readonly separatorChars = [";", ",", "\n"];
+  readonly separatorRegex = new RegExp(this.separatorChars.join("|"), "g");
 
   ngOnInit() {
-    this.listId = this.route.paramMap.pipe(map((params: ParamMap) => params.get('id') as string));
+    this.listId = this.route.paramMap.pipe(
+      map((params: ParamMap) => params.get("id") as string)
+    );
   }
 
-  add(event: MatChipInputEvent): void {
-    const input = event.input;
-    const value = event.value;
-
-    // Add our fruit
-    if ((value || '').trim()) {
-      this.users.push({ email: value.trim() });
-    }
-
-    // Reset the input value
-    if (input) {
-      input.value = '';
-    }
+  add(event: MatChipInputEvent) {
+    this.addEmails(event.value);
   }
 
   remove(user: any): void {
@@ -44,11 +44,45 @@ export class ListShareComponent implements OnInit {
     }
   }
 
+  onPaste(event: ClipboardEvent) {
+    event.preventDefault();
+    this.emailControl.patchValue(event.clipboardData.getData("text"));
+  }
+
+  addEmails(emails: string) {
+    let invalid = [];
+    const values = emails.split(this.separatorRegex);
+    for (const value of values) {
+      const trimmedValue = value.trim();
+      if (!trimmedValue) continue;
+      const { error, value: email } = this.validateEmail(trimmedValue);
+      if (error) {
+        invalid.push(email);
+      } else {
+        this.users.push({ email });
+      }
+    }
+    invalid.join(this.separatorChars[0] + " ");
+    this.emailControl.patchValue(invalid);
+  }
+
+  validateEmail(value: string) {
+    const form = new FormControl(undefined, [
+      Validators.email,
+      Validators.required
+    ]);
+
+    form.patchValue(value);
+    if (form.invalid) return { error: form.errors, value: value };
+
+    return { value, error: undefined as ValidationErrors };
+  }
+
   share(): void {
     if (this.users.length < 1) return;
     this.listId.subscribe(listId => {
       this.users.forEach(user => {
-        this.db.collection('invites').add({ listId: listId, to: user.email });
+        this.db.collection("invites").add({ listId: listId, to: user.email });
       });
     });
   }
